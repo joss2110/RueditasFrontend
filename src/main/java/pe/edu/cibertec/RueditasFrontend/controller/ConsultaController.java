94589b9c11d7e1e7952a2f3222cc91c11409a741
joss2110 <g.j.torres.perez@gmail.com>
2024-09-25 21:03:50 -0500

    font consulta listo y controlado casi listo

--- src/main/java/pe/edu/cibertec/RueditasFrontend/controller/ConsultaController.java
+++ src/main/java/pe/edu/cibertec/RueditasFrontend/controller/ConsultaController.java
@@ -3,7 +3,10 @@ package pe.edu.cibertec.RueditasFrontend.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pe.edu.cibertec.RueditasFrontend.viewmodel.RequestConsultaModel;
 
 @Controller
 @RequestMapping("/rueditas")
@@ -11,7 +14,25 @@ public class ConsultaController {
 
     @GetMapping("/inicio")
     public String inicio(Model model) {
-
+        RequestConsultaModel requestConsultaModel = new RequestConsultaModel("00","",
+                "","","","","");
+        model.addAttribute("requestConsultaModel", requestConsultaModel);
         return "inicio";
+    }
+    @PostMapping("/consulta")
+    public String autenticar(@RequestParam("placa") String placa, Model model) {
+
+        String placaPattern = "^[A-Za-z]{3}-\\d{3}$";
+        if (placa == null || placa.trim().length() == 0 || !placa.matches(placaPattern) ) {
+
+            RequestConsultaModel requestConsultaModel = new RequestConsultaModel("01", "Debe ingresar una placa correcta", "","","","","");
+            model.addAttribute("requestConsultaModel", requestConsultaModel);
+            return "inicio";
+        }
+
+
+        RequestConsultaModel requestConsultaModel = new RequestConsultaModel("00", "", "KIA","Sportage","5","26000","Negro");
+        model.addAttribute("requestConsultaModel", requestConsultaModel);
+        return "consulta";
     }
 }
--- src/main/java/pe/edu/cibertec/RueditasFrontend/viewmodel/RequestConsultaModel.java
+++ src/main/java/pe/edu/cibertec/RueditasFrontend/viewmodel/RequestConsultaModel.java
@@ -1,4 +1,8 @@
 package pe.edu.cibertec.RueditasFrontend.viewmodel;
 
-public record RequestConsultaModel() {
+import java.awt.*;
+
+public record RequestConsultaModel(String codigo, String mensaje, String marca,
+                                   String modelo, String nroAsientos, String precio,
+                                   String color) {
 }
